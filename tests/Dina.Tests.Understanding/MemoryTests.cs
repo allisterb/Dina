1e0c7cf86288d290ddcde148bf443aea9991aa93
Allister Beharry <allister.beharry@gmail.com>
2025-07-10 22:12:08 -0400

    Left out tests folder from scc by mistake.

---
+++ tests/Dina.Tests.Understanding/MemoryTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Dina.Tests.Understanding;
+
+using Serilog;
+using Serilog.Extensions.Logging;
+
+
+public class MemoryTests
+{
+    static MemoryTests()
+    {
+        var logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine(Runtime.AssemblyLocation, "Dina.log"))
+                .CreateLogger();
+        var lf = new SerilogLoggerFactory(logger);
+        var lp = new SerilogLoggerProvider(logger, false);
+        Runtime.Initialize("Dina.Understanding", "Tests", false, lf, lp);
+        Documents.MuPdfPath = "C:\\Projects\\Dina\\bin";
+    }
+
+    [Fact]
+    public async Task CanSearchMemory()
+    {
+        var m = new Memory(ModelRuntime.Ollama, OllamaModels.Gemma3n_2eb, OllamaModels.Nomic_Embed_Text);
+        var r = await m.ImportAsync("..\\..\\..\\..\\data\\test.pdf", "test");
+        Assert.True(r.IsSuccess);
+        var rx = await m.SearchAsync("data analysis", "test");
+        string s = "";
+        //Assert.NotNull(rx);
+        foreach (var response in rx.Value.Results)
+        {
+            Assert.NotNull(response.SourceName);
+            s += response;
+        }
+        Assert.NotNull(s);
+        Console.WriteLine(s);
+    }
+
+    [Fact]
+    public async Task CanAskMemory()
+    {
+        var m = new Memory(ModelRuntime.Ollama, OllamaModels.Gemma3n_2eb, OllamaModels.Nomic_Embed_Text);
+        var r = await m.ImportAsync("..\\..\\..\\..\\data\\test.pdf", "test");
+        Assert.True(r.IsSuccess);
+        var rx = m.AskAsync("What does the document say ?", "test");
+        string s = "";
+        Assert.NotNull(rx);
+        await foreach (var response in rx)
+        {
+            Assert.NotNull(response);
+            s += response;
+        }
+        Assert.NotNull(s);
+        Console.WriteLine(s);
+    }
+
+
+}
---
+++ tests/Dina.Tests.Vision/DocumentTests.cs
@@ -0,0 +1,51 @@
+namespace Dina.Tests.Vision;
+
+using Serilog;
+using Serilog.Extensions.Logging;
+
+public class DocumentTests
+{
+    static DocumentTests()
+    {
+        var logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine(Runtime.AssemblyLocation, "Dina.log"))
+                .CreateLogger();
+        var lf = new SerilogLoggerFactory(logger);
+        var lp = new SerilogLoggerProvider(logger, false);
+        Runtime.Initialize("Dina.Core.Understanding", "Tests", false, lf, lp);
+        Documents.MuPdfPath = "C:\\Projects\\Dina\\bin";
+    }
+
+    [Fact]
+    public void CanConvertPdfToText()
+    {
+        var r = Documents.ConvertPdfToText("..\\..\\..\\..\\data\\test3.pdf");
+        Assert.True(r.IsSuccess);
+        Assert.NotEmpty(r.Value);
+        Assert.Contains("6.7. Trends and business cycles", r.Value[0]); 
+    }
+
+    [Fact]
+    public void CanConvertPdf()
+    {
+        var r = Documents.ConvertPdfToImages("..\\..\\..\\..\\data\\test.pdf");
+        Assert.True(r.IsSuccess);
+        Assert.NotEmpty(r.Value);
+    }
+
+    [Fact]
+    public async Task CanGetScannerNames()
+    { 
+        var scanners = await Documents.GetScannerNames();
+        Assert.NotEmpty(scanners);  
+    }
+
+    [Fact]
+    public async Task CanScan()
+    {
+        var images = await Documents.Scan();
+        Assert.NotEmpty(images);
+    }
+}
